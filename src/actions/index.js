d55f6120a467bb5f7ef2f7610fa4c3b843f4f250
Equinox32 <tylerangelier@gmail.com>
2019-02-24 15:01:02 -0500

    Added .env support

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-const API_KEY = '921bd355a04ab84c2424cc10fae40135';
+require('dotenv').config();
+console.log(process.env.REACT_APP_WEATHER_API_KEY);
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
